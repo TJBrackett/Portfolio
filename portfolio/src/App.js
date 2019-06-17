f2d1cbf6d051b528bb7c91ced65161402f86efa2
Joseph M <joemark1989@gmail.com>
2019-06-16 22:45:44 -0700

    'Updated'

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,45 +1,98 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.css"
+import './App.css'
 
-function Navbar(){
+
+
+function Navbar() {
   return (
-  <div>navbar</div>
+    <React.Fragment>
+      <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div class="navbar-nav mr-auto">
+          </div>
+          <div class="form-inline my-5 my-lg-0">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item ml-3 mr-2">
+                <a class="nav-link" href="#header">Home</a>
+              </li>
+              <li class="nav-item ml-3 mr-2">
+                <a class="nav-link" href="#portfolio">Portfolio</a>
+              </li>
+              <li class="nav-item ml-3 mr-2">
+                <a class="nav-link" href="#about">About</a>
+              </li>
+              <li class="nav-item ml-3 mr-2">
+                <a class="nav-link" href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </React.Fragment>
   )
 }
 
-
-function Header(){
+function Header() {
   return (
-  <div>Header</div>
+    <article id="header" className="hero">
+      <div className="text-center py-5">
+        <h1 className="display-4">Hello, world!</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      </div>
+    </article>
   )
 }
 
-function Projects(){
+function Portfolio() {
   return (
-  <div>Projects</div>
+    <article id="portfolio" className="portfolio">
+      <div className="text-center py-5">
+        <h1 className="display-4">Portfolio</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      </div>
+    </article>
   )
 }
 
-function Experience(){
+function About() {
   return (
-  <div>Experience</div>
+    <article id="about" className="about">
+      <div className="text-center py-5">
+        <h1 className="display-4">About</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      </div>
+    </article>
   )
 }
 
-function Footer(){
+function Contact() {
   return (
-  <div>Footer</div>
+    <article id="contact" className="footer">
+      <div className="text-center py-5">
+        <h1 className="display-4">Contact</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      </div>
+    </article>
   )
 }
 
 function App() {
   return (
     <React.Fragment>
-    <Navbar/>
-    <Header/>
-    <Projects/>
-    <Experience/>
-    <Footer/>
+      <Navbar />
+      <Header />
+      <Portfolio />
+      <About />
+      <Contact />
     </React.Fragment>
   )
 }
