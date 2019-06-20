11c2af51e52574405eca4be7759a4512ee086fcd
Joseph M <joemark1989@gmail.com>
2019-06-19 22:40:09 -0700

    fixed

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <React.Fragment>
       <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-        <img src="" alt="initals"/>
+        <img src="" alt="initals" />
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
