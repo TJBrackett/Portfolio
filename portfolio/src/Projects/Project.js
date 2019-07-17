37bf5b08de24939649bed910f0f86966526332e9
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-07-17 02:28:30 -0700

    Changed card

--- portfolio/src/Projects/Project.js
+++ portfolio/src/Projects/Project.js
@@ -9,19 +9,23 @@ function Project(title, description, img, pageLink, githubLink) {
   }
 
   return (
+    <React.Fragment>
     <div id="projCard">
       <div className="card bg-color border-dark border rounded-0 m-3 p-0">
-        <h5 className="card-title text-center mt-2">{title}</h5>
+      <div  id="projImg">
         <img className="img-fluid card-img-top" src={pic} alt="projectPic" />
-        <div className="card-body text-centered">
-          <p className="card-text  text-center">{description}</p>
+        <div  id="projTop">
+        <h4 className="card-title text-center mt-2">{title}</h4>
+        <p className="card-text  text-center">{description}</p>
         </div>
-        <div className="btn-group" role="group">
+        <div className="btn-group" role="group"id="projBtn">
           <a type="button" className="border rounded-0 border-dark btn-secondary btn-outline-dark btn-lg col-6 text-center" href={githubLink} target="_blank" rel="noopener noreferrer">Github</a>
           <a type="button" className="border rounded-0 border-dark btn-secondary btn-outline-dark btn-lg col-6 text-center" href={pageLink} target="_blank" rel="noopener noreferrer">Website</a>
         </div>
+        </div>
       </div>
     </div>
+    </React.Fragment>
   );
 }
 
