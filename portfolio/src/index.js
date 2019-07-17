1ddfb7ef3db06b8e84e20d23b1ec982f51263afc
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-07-16 20:35:56 -0700

    Update index.js

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -5,16 +5,16 @@ import './index.css'
 import App from './App/App.js';
 import * as serviceWorker from './serviceWorker'
 import Contact from './Contact/Contact.js'
-import { HashRouter, Route, Switch } from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
 import About from "./About/About.js"
 
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter>
         <Route exact path='/' component={App} />
         <Route exact path='/contact' component={Contact} />
         <Route extact path='/about' component={About} />
-    </HashRouter>,
+    </BrowserRouter>,
     document.getElementById('root')
 )
 
