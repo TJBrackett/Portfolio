c3c7f37eedbf2fb46f2fea42b8a4a2ff338f99b9
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-07-18 14:19:18 -0700

    Update server.js

--- portfolio/backend/server.js
+++ portfolio/backend/server.js
@@ -60,13 +60,13 @@ app.post('/', (req, res) => {
 httpProxy.createServer({
     target: {
         host: 'myemailbot.tk',
-        port: '443'
-    },
-    ssl: {
-        key: fs.readFileSync(__dirname + '/key.pem', 'utf8'),
-        cert: fs.readFileSync(__dirname + '/cert.pem', 'utf8')
+        port: '80'
     }
-  }).listen(443);
+    // ssl: {
+    //     key: fs.readFileSync(__dirname + '/key.pem', 'utf8'),
+    //     cert: fs.readFileSync(__dirname + '/cert.pem', 'utf8')
+    // }
+  }).listen(80);
 
 // app.set('port' ,(process.env.PORT || 9521));
 // http.createServer( app).listen(app.get('port'), () => {
