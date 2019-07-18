dd64525a6f2b5ecbcad4ba49f2036b82e5031e4e
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-07-18 10:43:39 -0700

    Update Contact.js

--- portfolio/src/Contact/Contact.js
+++ portfolio/src/Contact/Contact.js
@@ -47,7 +47,7 @@ class Contact extends React.Component {
     handleSubmit(event) {
         event.preventDefault();
 
-        fetch("https://45.79.34.27:8888", {
+        fetch("https://45.79.34.27:443", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
