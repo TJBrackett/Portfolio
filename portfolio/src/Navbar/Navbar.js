2349f8d5bff625d1a269ff8e0ad6066176dba72c
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-08-11 22:01:55 -0700

    More style changes.

--- portfolio/src/Navbar/Navbar.js
+++ portfolio/src/Navbar/Navbar.js
@@ -7,7 +7,7 @@ import Logo from "../Images/initial.png"
 function Navbar() {
   return (
     <React.Fragment>
-      <nav className="nav-bg navbar sticky-top navbar-expand-lg navbar-light">
+      <nav className="nav-bg navbar sticky-top navbar-expand-md navbar-light">
         <Link to="/"><img className="pic" src={Logo} alt="logo" /></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -20,6 +20,7 @@ function Navbar() {
               <li className="nav-item">
                 <Link className="link nav-link text-white font-weight-bold" to="/">
                   <div className="navLink">
+                  <i className="fa fa-lg fa-home fa-3x" /><br></br>
                     Home
                     </div>
                 </Link>
@@ -27,6 +28,7 @@ function Navbar() {
               <li className="nav-item">
                 <Link className="link nav-link text-white font-weight-bold" to={Resume} target="_blank" rel="noopener noreferrer">
                   <div className="navLink">
+                  <i className="fa fa-lg fa-file fa-3x" /><br></br>
                     Resume
                     </div>
                 </Link>
@@ -35,6 +37,7 @@ function Navbar() {
                 
                 <Link className="link nav-link text-white font-weight-bold" to="/about">
                   <div className="navLink">
+                  <i className="fa fa-lg fa-user fa-3x" /><br></br>
                     About
                     </div>
                 </Link>
@@ -42,6 +45,7 @@ function Navbar() {
               <li className="nav-item">
                 <Link className="link nav-link text-white font-weight-bold" to="/contact">
                   <div className="navLink">
+                  <i className="fa fa-lg fa-comments fa-3x" /><br></br>
                     Contact
                     </div>
                 </Link>
