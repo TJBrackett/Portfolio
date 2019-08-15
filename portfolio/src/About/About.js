ee2d65a1afd28c1723d562beddb75f57bdef5268
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-08-14 20:30:03 -0700

    More about styling

--- portfolio/src/About/About.js
+++ portfolio/src/About/About.js
@@ -32,67 +32,67 @@ function About() {
                 <h2 className="aboutTech">Technologies I have worked with:</h2>
                 <div className="p-5 d-flex justify-content-center flex-wrap">
                   <div>
-                    <img className="p-0" src={Angular} alt="Angular"></img><br></br>
+                    <img className="aboutIcon p-0" src={Angular} alt="Angular"></img><br></br>
                     <h5>Angular</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Assembly} alt="Assembly"></img><br></br>
+                    <img className="aboutIcon p-0" src={Assembly} alt="Assembly"></img><br></br>
                     <h5>Assembly</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Bash} alt="Bash"></img><br></br>
+                    <img className="aboutIcon p-0" src={Bash} alt="Bash"></img><br></br>
                     <h5>Bash</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Bootstrap} alt="Bootstrap"></img><br></br>
+                    <img className="aboutIcon p-0" src={Bootstrap} alt="Bootstrap"></img><br></br>
                     <h5>Bootstrap</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Cpp} alt="Cpp"></img><br></br>
+                    <img className="aboutIcon p-0" src={Cpp} alt="Cpp"></img><br></br>
                     <h5>C++</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Css} alt="Css"></img><br></br>
+                    <img className="aboutIcon p-0" src={Css} alt="Css"></img><br></br>
                     <h5>CSS3</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Git} alt="Git"></img><br></br>
+                    <img className="aboutIcon p-0" src={Git} alt="Git"></img><br></br>
                     <h5>Git</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Html} alt="Html"></img><br></br>
+                    <img className="aboutIcon p-0" src={Html} alt="Html"></img><br></br>
                     <h5>HTML5</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Java} alt="Java"></img><br></br>
+                    <img className="aboutIcon p-0" src={Java} alt="Java"></img><br></br>
                     <h5>Java</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Js} alt="Js"></img><br></br>
+                    <img className="aboutIcon p-0" src={Js} alt="Js"></img><br></br>
                     <h5>Javascript</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Linux} alt="Linux"></img><br></br>
+                    <img className="aboutIcon p-0" src={Linux} alt="Linux"></img><br></br>
                     <h5>Linux</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Node} alt="Node"></img><br></br>
+                    <img className="aboutIcon p-0" src={Node} alt="Node"></img><br></br>
                     <h5>NodeJS</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Python} alt="Python"></img><br></br>
+                    <img className="aboutIcon p-0" src={Python} alt="Python"></img><br></br>
                     <h5>Python</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={ReactIcon} alt="React"></img><br></br>
+                    <img className="aboutIcon p-0" src={ReactIcon} alt="React"></img><br></br>
                     <h5>React</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Sql} alt="Sql"></img><br></br>
+                    <img className="aboutIcon p-0" src={Sql} alt="Sql"></img><br></br>
                     <h5>SQL</h5>
                   </div>
                   <div>
-                    <img className="p-0" src={Vb} alt="Vb"></img><br></br>
+                    <img className="aboutIcon p-0" src={Vb} alt="Vb"></img><br></br>
                     <h5>Visual Basic</h5>
                   </div>
                 </div>
