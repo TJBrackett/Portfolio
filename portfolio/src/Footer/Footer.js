1112f81701f1fa86b0d3a272cd569555c6fcf3b7
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-08-17 12:36:18 -0700

    Fixing errors for Google Lighthouse

--- portfolio/src/Footer/Footer.js
+++ portfolio/src/Footer/Footer.js
@@ -8,16 +8,16 @@ function Footer() {
     <React.Fragment>
       <div className="container-fluid text-center footer text-white py-2">
         <div>
-          <a className="footer-link" href="https://twitter.com/TJBrackett" target="_blank" rel="noopener noreferrer">
-            < i className="fa fa-twitter fa-3x m-3" />
+          <a className="footer-link" href="https://twitter.com/TJBrackett" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+            <i className="fa fa-twitter fa-3x m-3" alt="twitter" />
           </a>
-          <a className="footer-link" href="https://www.linkedin.com/in/tjbrackett/" target="_blank" rel="noopener noreferrer">
+          <a className="footer-link" href="https://www.linkedin.com/in/tjbrackett/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
             <i className="fa fa-linkedin fa-3x m-3" />
           </a>
-          <a className="footer-link" href="https://stackoverflow.com/users/11122711/tj-brackett" target="_blank" rel="noopener noreferrer">
+          <a className="footer-link" href="https://stackoverflow.com/users/11122711/tj-brackett" target="_blank" rel="noopener noreferrer" aria-label="Stack Overflow">
             <i className="fa fa-stack-overflow fa-3x m-3" />
           </a>
-          <a className="footer-link" href="https://github.com/TJBrackett" target="_blank" rel="noopener noreferrer">
+          <a className="footer-link" href="https://github.com/TJBrackett" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
             <i className="fa fa-lg fa-github fa-3x m-3" />
           </a>
         </div>
--- portfolio/src/Projects/Project.js
+++ portfolio/src/Projects/Project.js
@@ -33,18 +33,18 @@ function Project(title, description, img, pageLink, githubLink, date) {
 
   //try just doing a card for more info on click
   return (
-      <div id="projCard">
+      <div className="projCard">
         <div className="card bg-color border-dark border rounded-0 m-0 p-0">
-          <div id="projImg">
+          <div className="projImg">
             <img className="img-fluid card-img-top" src={pic} alt="projectPic" />
             {/* <div id="projTop">
               <button type="button" className="moreInfoBtn border rounded-0 border-dark btn-secondary btn-outline-dark btn-lg col-12 text-center" data-toggle="modal" data-target="#projModal" onClick={() => updateInfo()}>{info} Info</button>
             </div> */}
-            <div id="projBottom">
+            <div className="projBottom">
               <h5 className="card-title text-center mt-2">{title}</h5>
-              <p className="card-text  text-center text-wrap">{date} - {description}</p>
+              <p className="card-text text-center text-wrap">{date} - {description}</p>
             </div>
-            <div className="btn-group" role="group" id="projBtn">
+            <div className="btn-group projBtn" role="group">
               <a type="button" className="githubBtn border rounded-0 border-dark btn-secondary btn-outline-dark btn-lg col-6 text-center" href={githubLink} target="_blank" rel="noopener noreferrer">Github</a>
               <a type="button" className="pageLinkBtn border rounded-0 border-dark btn-secondary btn-outline-dark btn-lg col-6 text-center" href={pageLink} target="_blank" rel="noopener noreferrer">Website</a>
             </div>
