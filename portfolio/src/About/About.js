864750a8b6caa088596970f5032e95f383ef753a
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-10-10 00:30:37 -0700

    More ios issues to fix

--- portfolio/src/About/About.js
+++ portfolio/src/About/About.js
@@ -116,7 +116,7 @@ export function About() {
             <div className="col-12">
               <h2 className="aboutTech">What I'm up to now</h2>
             </div>
-            <TwitterTimelineEmbed className="tweets"
+            <TwitterTimelineEmbed
               sourceType="profile"
               screenName="TJBrackett"
               options={{
