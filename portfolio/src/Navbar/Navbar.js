3e3827b78b880fa6eb6dd92ec4b0b158b018ceb2
TJ Brackett <51838812+TJBrackett@users.noreply.github.com>
2019-10-09 21:44:51 -0700

    Navbar size change broke offset on mobile

--- portfolio/src/Navbar/Navbar.js
+++ portfolio/src/Navbar/Navbar.js
@@ -6,7 +6,7 @@ import A from "react-anchor-link-smooth-scroll"
 function Navbar() {
   return (
       <nav className="nav-bg navbar sticky-top navbar-expand-md navbar-light">
-        <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler ml-auto mt-3 mb-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="navbar collapse navbar-collapse" id="navbarSupportedContent">
