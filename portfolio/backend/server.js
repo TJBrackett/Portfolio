a1148237f92ce860355617b15992d3a5995357fe
TJ Brackett <tj@brackett.dev>
2020-02-09 00:07:05 -0800

    Still trying to fix emailer.

--- portfolio/backend/server.js
+++ portfolio/backend/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(json());
+app.use(express.json());
 
 app.post('/', (req, res) => {
     let name = req.body.name;
